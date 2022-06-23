b24b4ff9380fe3e4cf1c0001454e3568c035c1e1
Thanuja Alavala <thanujaalavala@Thanujas-MacBook-Air.local>
2022-06-23 12:20:40 +0530

    new file added

---
+++ backend.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const url = require("url");
+const bodyParser = require("body-parser");
+
+const port = 4000;
+
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(upload());
+app.use(bodyParser.json());
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, "/build")));
+
+app.use(express.json());
+
+app.get("/health-check", (req, res) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: "Ok",
+    timestamp: Date.now(),
+  };
+  res.send(healthcheck);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/'));
+}); 
+
+app.listen(port, () => {
+  console.log("********************************************");
+  console.log("Listening on PORT 4000");
+  console.log("********************************************");
+});
